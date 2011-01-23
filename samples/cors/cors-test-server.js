c3426a0890fc90e1d2f3f0915116d321b22e8d97
Monsur Hossain <monsur@gmail.com>
2011-01-23 17:05:31 -0600

    Basic static server infrastructure

---
+++ samples/cors/cors-test-server.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+
+// Retrieve the port from the --port command line parameter.
+// If --port is not specified, return the default port.
+// This is copied from echo-server.js, it should eventually be consolidated.
+function getPort() {
+  var argv = process.argv;
+  for (var i = 0; i < argv.length; i++) {
+    if (argv[i] == '--port') {
+      return parseInt(argv[i+1]);
+    }
+  }
+  return 8125;
+}
+
+var port = getPort();
+var filename = './default.htm';
+
+var contents = null;
+function loadFileContents() {
+  contents = fs.readFileSync(filename);
+}
+loadFileContents();
+fs.watchFile(filename, {interval: 2000}, loadFileContents);
+
+require('http').createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end(contents);
+}).listen(port);
+
+console.log('Server running at http://127.0.0.1:' + port + '/');
+
